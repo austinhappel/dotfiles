7944bcf03106fc17fded24286d515a4df80292a7
Austin Happel <austin.happel@smashingideas.com>
2013-06-10 16:34:57 -0700

    added java_home var

--- personal/osx.sh
+++ personal/osx.sh
@@ -9,3 +9,6 @@ function tabc() {
   NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
   osascript -e "tell application \"Terminal\" to set current settings of front window to settings set \"$NAME\""
 }
+
+# java_home var. Backticks are required.
+export JAVA_HOME=`/usr/libexec/java_home`
