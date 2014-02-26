96b4514f06b2eea0bff12177c7f79df842fe0da6
Austin Happel <austin.happel@smashingideas.com>
2014-02-26 10:52:26 -0800

    Removed tabc, never use it.  Added myip.
    Myip returns just the IP address(es) of your computer.
    
    Specify enter the commit message for your changes. Lines starting

--- personal/zsh/osx.sh
+++ personal/zsh/osx.sh
@@ -3,9 +3,3 @@
 
 # set 64 bit architecture
 export ARCHFLAGS="-arch x86_64"
-
-# change the color scheme of the current terminal tab
-function tabc() {
-  NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
-  osascript -e "tell application \"Terminal\" to set current settings of front window to settings set \"$NAME\""
-}
