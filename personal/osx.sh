288a1fe65d206037ff0945a8d3893c2a3d6220a5
Austin Happel <austin@austinhappel.com>
2015-12-29 11:25:37 -0800

    added pyenv shim/autocomplete to OSX script, assuming pyenv is installed via homebrew

--- personal/osx.sh
+++ personal/osx.sh
@@ -6,3 +6,6 @@ export ARCHFLAGS="-arch x86_64"
 
 # java_home var. Backticks are required.
 export JAVA_HOME=`/usr/libexec/java_home`
+
+# Enables autocomplete/shims for homebrew-installed pyenv.
+if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
