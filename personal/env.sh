321e06bc4c216c6d7c225e90f7c0a88ff4e31bcb
Austin Happel <austin.happel@23294-mbpr.2d.hq>
2017-03-16 09:20:24 -0700

    removed todo

--- personal/env.sh
+++ personal/env.sh
@@ -16,10 +16,3 @@ export npm_config_prefix=~/local
 # requires that virtualenvwrapper is installed globally
 # (sudo pip install virtualenvwrapper)
 # source /usr/local/bin/virtualenvwrapper.sh
-
-# todo
-source ${DOTFILES_DIR}/personal/tools/todo/todo_completion
-export TODOTXT_CFG_FILE="${DOTFILES_DIR}/personal/tools/todo/todo.cfg"
-alias todo="${DOTFILES_DIR}/personal/tools/todo/todo.sh"
-alias t='todo'
-
