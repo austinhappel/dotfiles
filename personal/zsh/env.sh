294158035015e0f0cb3d6fd24e6f0e10e3c69700
Austin Happel <austin@austinhappel.com>
2018-03-13 13:38:41 -0700

    comment out zsh prompts (work computer)

--- personal/zsh/env.sh
+++ personal/zsh/env.sh
@@ -12,10 +12,10 @@ export CLICOLOR='true'
 autoload -U colors && colors
 
 # useful git info when in git repository
-source ~/dotfiles/personal/zsh/tools/zsh-git-prompt/zshrc.sh
+# source ~/dotfiles/personal/zsh/tools/zsh-git-prompt/zshrc.sh
 
 # prompt
-PROMPT='%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg_no_bold[yellow]%}%1~ %{$reset_color%}% $(git_super_status) %# '
+# PROMPT='%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg_no_bold[yellow]%}%1~ %{$reset_color%}% $(git_super_status) %# '
 
 # auto completion
 autoload -U compinit
