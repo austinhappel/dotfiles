c044156d19474d501f0c5c1f5c88713c548f9547
Austin Happel <austin@austinhappel.com>
2020-04-06 12:20:17 -0700

    Add brewfile, tweaks to osx, etc

--- personal/osx.sh
+++ personal/osx.sh
@@ -7,8 +7,3 @@ export ARCHFLAGS="-arch x86_64"
 # java_home var. Backticks are required.
 export JAVA_HOME=`/usr/libexec/java_home`
 
-# Enables autocomplete/shims for homebrew-installed pyenv.
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-if which pyenv-virtualenv-init > /dev/null; then 
-	eval "$(pyenv virtualenv-init -)";
-fi
